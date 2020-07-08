04be631e9d31998d504b257b9a390d130c4edef2
SignpostMarv <me@signpostmarv.name>
2020-07-08 15:28:11 +0100

    incomplete production count implementation

--- src/scripts/ui.ts
+++ src/scripts/ui.ts
@@ -14,8 +14,12 @@ const items = (datasource.items as any) as named_item_dict;
 const recipe_keys = Object.keys(recipes);
 const item_keys = Object.keys(items);
 
-declare type named_item = {name: string};
+declare type named_item = {
+	className: string,
+	name: string
+};
 declare type named_item_dict = {[key: string]: named_item};
+declare type item_categories = 'raw_resources'|'ingots'|'constructables'|'assemblerables'|'manufacturerables'|'refineables'|'handcraftables';
 
 const datamapped = {
 	"raw_resources": {} as named_item_dict,
@@ -27,6 +31,10 @@ const datamapped = {
 	"handcraftables": {} as named_item_dict,
 };
 
+const mappeddata: {[key: string]: item_categories} = {};
+
+const mappedids: string[] = [];
+
 const datamappedlabels = {
 	"raw_resources": "Raw Resources",
 	"ingots": "Ingots",
@@ -37,12 +45,16 @@ const datamappedlabels = {
 	"handcraftables": "Handcraftables"
 };
 
-(Object.keys(datamapped) as ('raw_resources'|'ingots'|'constructables'|'assemblerables'|'manufacturerables'|'refineables'|'handcraftables')[]).forEach((category) => {
+(Object.keys(datamapped) as item_categories[]).forEach((category) => {
 	data[category].forEach((id) => {
 		if (recipe_keys.includes(id)) {
 			datamapped[category][id] = (recipes[id] as named_item);
+			mappeddata[id] = category;
+			mappedids.push(id);
 		} else if (item_keys.includes(id)) {
 			datamapped[category][id] = (items[id] as named_item);
+			mappeddata[id] = category;
+			mappedids.push(id);
 		} else {
 			throw new Error(`could not find ${id} in recipes or items!`);
 		}
@@ -57,6 +69,9 @@ export class UserInterface extends LitElement
 	@property()
 	counter: number = 0;
 
+	@property()
+	production_plan: WeakMap<named_item, number> = new WeakMap();
+
 	createRenderRoot(): UserInterface
 	{
 		++counter;
@@ -68,6 +83,14 @@ export class UserInterface extends LitElement
 
 	render(): TemplateResult
 	{
+		const mappedlabels = (
+			Object.entries(datamappedlabels) as [item_categories, string][]
+		);
+
+		const item_in_production_plan = (item: named_item) => {
+			return this.production_plan.has(item);
+		};
+
 		return html`
 			<section class="item-list">
 				<header>
@@ -79,7 +102,7 @@ export class UserInterface extends LitElement
 					@keyup=${this.handleItemListSearch}
 				>
 				<output for="item-search-${this.counter}">
-					${(Object.entries(datamappedlabels) as ['raw_resources'|'ingots'|'constructables'|'assemblerables'|'manufacturerables'|'refineables'|'handcraftables', string][]).map((entry) => {
+					${mappedlabels.map((entry) => {
 						const [id, label] = entry;
 						const items = Object.entries(datamapped[id]);
 
@@ -99,6 +122,7 @@ export class UserInterface extends LitElement
 												<button
 													type="button"
 													value="${item[0]}"
+													@click=${this.handleItemClick}
 												>${item[1].name}</button>
 											</li>
 										`;
@@ -108,6 +132,62 @@ export class UserInterface extends LitElement
 						`;
 					})}
 				</output>
+			</section>
+			<section class="production-planner">
+				${mappedlabels.filter((entry) => {
+					const [id] = entry;
+
+					return Object.values(datamapped[id]).some(
+						item_in_production_plan
+					);
+				}).map((entry) => {
+					const [id, label] = entry;
+					const items = Object.values(datamapped[id]).filter(
+						item_in_production_plan
+					);
+
+					items.sort((a, b) => {
+						return a.name.localeCompare(b.name);
+					});
+
+					return html`
+						<table>
+							<caption>${label}</caption>
+							<thead>
+								<tr>
+									<th>Item</th>
+									<th>
+										<abbr
+											title="Units Per Minute"
+										>upm</abbr>
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								${items.map((item) => {
+									return html`
+										<tr>
+											<th scope="row">
+												${item.name}
+											</th>
+											<td>
+												<input
+													name="${item.className}"
+													value="${
+														this.production_plan.get(item)
+													}"
+													@change=${
+														this.handleProductionItemChange
+													}
+												>
+											</td>
+										</tr>
+									`;
+								})}
+							</tbody>
+						</table>
+					`;
+				})}
 			</section>
 		`;
 	}
@@ -126,5 +206,28 @@ export class UserInterface extends LitElement
 				item.classList.add('does-not-match-query')
 			}
 		});
+	}
+
+	private handleItemClick(e: Event): void
+	{
+		const value = (e.target as HTMLButtonElement).value;
+
+		if ( ! mappedids.includes(value))
+		{
+			throw new Error('Could not find item ref!');
+		}
+
+		const item = datamapped[mappeddata[value]][value];
+
+		if ( ! this.production_plan.has(item)) {
+			this.production_plan.set(item, 0);
+		}
+
+		this.requestUpdate();
+	}
+
+	private handleProductionItemChange(_e: Event): void
+	{
+		console.log('not yet implemented');
 	}
 }
